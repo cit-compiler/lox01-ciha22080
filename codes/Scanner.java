7ac465d1b28d01c4e739d001bfabe2280f3f7da0
ciha22080 <ciha22080@g.nihon-u.ac.jp.>
2025-12-16 11:36:48 +0900

    add main logic for expression parser

--- codes/Scanner.java
+++ codes/Scanner.java
@@ -16,22 +16,22 @@ class Scanner {
 
   private static final Map<String, TokenType> keywords = new HashMap<>();
   static {
-    keywords.put("and",    AND);
-    keywords.put("class",  CLASS);
-    keywords.put("else",   ELSE);
-    keywords.put("false",  FALSE);
-    keywords.put("for",    FOR);
-    keywords.put("fun",    FUN);
-    keywords.put("if",     IF);
-    keywords.put("nil",    NIL);
-    keywords.put("or",     OR);
-    keywords.put("print",  PRINT);
-    keywords.put("return", RETURN);
-    keywords.put("super",  SUPER);
-    keywords.put("this",   THIS);
-    keywords.put("true",   TRUE);
-    keywords.put("var",    VAR);
-    keywords.put("while",  WHILE);
+    keywords.put("and",    TokenType.AND);
+    keywords.put("class",  TokenType.CLASS);
+    keywords.put("else",   TokenType.ELSE);
+    keywords.put("false",  TokenType.FALSE);
+    keywords.put("for",    TokenType.FOR);
+    keywords.put("fun",    TokenType.FUN);
+    keywords.put("if",     TokenType.IF);
+    keywords.put("nil",    TokenType.NIL);
+    keywords.put("or",     TokenType.OR);
+    keywords.put("print",  TokenType.PRINT);
+    keywords.put("return", TokenType.RETURN);
+    keywords.put("super",  TokenType.SUPER);
+    keywords.put("this",   TokenType.THIS);
+    keywords.put("true",   TokenType.TRUE);
+    keywords.put("var",    TokenType.VAR);
+    keywords.put("while",  TokenType.WHILE);
   }
 
   Scanner(String source) {
